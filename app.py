9fd647a8800af1968af45593c7137c4cb2a3f62d
Oppai1442 <61798788+Oppai1442@users.noreply.github.com>
2025-02-01 02:30:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import sys
 from PyQt5.QtWidgets import (QApplication)
+from PyQt5.QtGui import QIcon
+from resources import *
 
 from models.OverlayManager import OverlayManager
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(":icon.ico"))
     window = OverlayManager()
     window.show()
     sys.exit(app.exec_())
